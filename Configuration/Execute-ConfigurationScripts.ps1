8406ef8bf73b7c43434702c4b3d3b2157fd203b5
jakedenyer-s24 <35513506+jakedenyer-s24@users.noreply.github.com>
2018-01-18 15:54:58 -0500

    Update Execute-ConfigurationScripts.ps1

--- Configuration/Execute-ConfigurationScripts.ps1
+++ Configuration/Execute-ConfigurationScripts.ps1
@@ -4,7 +4,7 @@ param (
     [string]$UserPass,
     [string]$ApiKey = (New-Guid | Select-Object -ExpandProperty Guid),
     [string]$EmailAddress = 'First.Last@Domain.com',
-    [string]$PrivateGalleryName = 'sdiscover'
+    [string]$PrivateGalleryName = 'PSPrivateGallery'
 )
 
 Push-Location $PSScriptRoot
